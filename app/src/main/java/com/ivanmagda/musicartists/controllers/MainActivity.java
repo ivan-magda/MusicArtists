08dfcb236ac7de0068de05e27ec3c403099f3915
Ivan Magda <imagda15@gmail.com>
2016-03-28 00:40:49 +0900

    Remove options menu.

--- app/src/main/java/com/ivanmagda/musicartists/controllers/MainActivity.java
+++ app/src/main/java/com/ivanmagda/musicartists/controllers/MainActivity.java
@@ -7,15 +7,13 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.ivanmagda.musicartists.view.DividerItemDecoration;
-import com.ivanmagda.musicartists.view.MusicArtistRecyclerViewAdapter;
 import com.ivanmagda.musicartists.R;
 import com.ivanmagda.musicartists.api.ArtistHttpApi;
 import com.ivanmagda.musicartists.model.Artist;
+import com.ivanmagda.musicartists.view.DividerItemDecoration;
+import com.ivanmagda.musicartists.view.MusicArtistRecyclerViewAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -60,28 +58,6 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, "You are offline. Please connect to the internet.",
                     Toast.LENGTH_SHORT).show();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     // DownloadMusicArtistsTask.
